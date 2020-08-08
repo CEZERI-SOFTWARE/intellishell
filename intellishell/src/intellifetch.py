ddb669c036a8f5d2bb0f96d11b7689048d7c9931
Emir Ensar <64320256+rahmanlar@users.noreply.github.com>
2020-08-08 09:46:50 +0300

    Update intellifetch.py

--- intellishell/src/intellifetch.py
+++ intellishell/src/intellifetch.py
@@ -6,85 +6,91 @@ import sys
 import sysconfig
 import random
 import platform
+import distro
 import subprocess
 import cmd
 import ssl
+import distro
+import cpuid
+import GPUtil
+import csv
 import datetime
 import time
 
+class colors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+version = "1.0.0"
+hostname = socket.gethostname()
+username = getpass.getuser()
+suankizaman = datetime.datetime.now()
+
 def fetch():
-    print(bcolors.OKGREEN + username + bcolors.ENDC + "'s computer")
-    print("Name: " + bcolors.OKGREEN  + hostname + bcolors.ENDC) 
+    print(colors.BLUE + "Intellifetch " + version + colors.END)
+    print()
 def fetch_os():
-      if platform.system() == "Darwin":
-        print("""
-                        .888"
-                      .8888'"
-                    .8888'"
-                    888'"
-                    8'"
-        .88888888888. .88888888888."
-    .8888888888888888888888888888888." 
-  .8888888888888888888888888888888888." 
-  &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
-  &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
-  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:"
-  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:"
-  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:"
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%."
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%."
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%."
-  00000000000000000000000000000000000"
-    000000000000000000000000000000000"
-    0000000000000000000000000000000"
-      ###########################"
-      #######################"
-          #################"
-        
-  """)
+    if platform.system() == "Darwin":
+     print("""
+                      .888
+                    .8888'
+                   .8888'
+                   888'
+                   8'
+      .88888888888. .88888888888.
+   .8888888888888888888888888888888.
+ .8888888888888888888888888888888888.
+.&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
+&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
+&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%.
+ 00000000000000000000000000000000000
+  000000000000000000000000000000000
+   0000000000000000000000000000000
+     ###########################
+     #######################
+         #################
+      
+""")
      print("Kernel Name: Darwin")
      print("OS name: MacOS")
     elif platform.system() == "Windows":
-      print("""
-             _.-;;-._
-      '-..-'|   ||   |
-      '-..-'|_.-;;-._|
-      '-..-'|   ||   |
-      '-..-'|_.-''-._|
-""")
-      print("Kernel Name: ")
-      print("NT")
-      print("OS Name: ")
-      print("Windows")
-      print("Release:")
-      print(platform.release())
-    elif platform.system() == "Linux":
-      print("""
-        dGGGGMMb
-       @p~qp~~qMb  
-       M|@||@) M|
-       @,----.JM|
-      JS^\__/  qKL   
-     dZP        qKRb
-    dZP          qKKb
-   fZP            SMMb
-   HZM            MMMM
-   FqM            MMMM
- __| ".        |\dS"qML
- |    `.       | `' \Zq
-_)      \.___.,|     .'
-\____   )MMMMMP|   .'
-     `-'       `--' 
-
-""")
-      print("Kernel: " + bcolors.OKGREEN)
-      print("Linux" + bcolors.ENDC)
-      print("Kernel Release: ")
-      print(bcolors.OKGREEN + platform.release() + bcolors.ENDC)
-      print("Distribution: " + bcolors.OKGREEN)
-      os.system(distribution)
-
       
-def fetch_etc():
-  print(bcolors.ENDC + "Platform:")
-  print(bcolors.OKGREEN + sysconfig.get_platform() + bcolors.ENDC)
+     print("       _.-;;-._     " + colors.GREEN + username + colors.END + "'s " +colors.GREEN + hostname + colors.END)
+     print("-..--'|   ||   |    " + "--------------")
+     print("'-..-'|_.-;;-._|    " + "Kernel Name: ")
+     print("'-..-'|   ||   |    " + colors.GREEN + "NT" + colors.END)   
+     print("'-..-'|_.-''-._|    " + "OS Name: ")
+     print("                    " + colors.GREEN + "Windows" + colors.END)
+     print("                    " + "Release: ")
+     print("                    " + colors.GREEN +  platform.release() + colors.END)
+
+    elif platform.system() == "Linux":
+      
+      print("      dGGGGMMb")        
+      print("      p~qp~~qMb     " + colors.GREEN + username + colors.END + "'s " +colors.GREEN + hostname + colors.END)
+      print("      M|@||@)M|     " + "--------------")
+      print("      @,----.JM|     " + "Kernel: ")
+      print("     JS^\__/  qKL    " + colors.GREEN + " Linux " + colors.END +  platform.release())
+      print("    dZP        qKRb  " + "Distribution: ")
+      print("   dZP          qKKb  " + colors.GREEN + distro.id() + colors.END)
+      print("  fZP            SMMb " + colors.END + "Platform:")
+      print("  HZM            MMMM " + colors.GREEN + sysconfig.get_platform() + colors.END)
+      print("  FqM            MMMM     ")
+      print("  __|          \dSqML     ")
+      print(" |    `.       | `' \Zq    ")
+      print("_)      \.___.,|     .'     ")
+      print("\____   )MMMMMP|   .'    ")
+      print("     `-'       `--'      ")
+  
